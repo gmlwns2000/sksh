802d6c53c1667a0bf3aaba0bd38b33086256f51d
AinL <gmlwns5176@gmail.com>
2018-02-23 18:07:21 +0900

    first commit

---
+++ sksh/CodePreProcessor.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sksh
+{
+    public class CodePreProcessor
+    {
+        static char[] Trims = new char[] { ' ', '\t' };
+
+        public List<string> Content { get; set; }
+        public List<string> Defines { get; set; } = new List<string>();
+
+        public string FilePath { get; set; }
+
+        public CodePreProcessor(string filename)
+        {
+            var file = filename.Replace('/', '\\');
+            if (!Path.IsPathRooted(file))
+            {
+                file = Path.Combine(Environment.CurrentDirectory, file);
+            }
+
+            if (!File.Exists(file))
+                throw new Exception($"File is not found. {file}");
+
+            Content = File.ReadAllLines(file).ToList();
+            FilePath = file;
+
+            Console.WriteLine($"Opened : {FilePath}");
+        }
+
+        public CodePreProcessor(List<string> content, List<string> defines, string filepath)
+        {
+            Content = content;
+            Defines = defines;
+            FilePath = filepath;
+        }
+
+        public void Run()
+        {
+            bool skipline = false;
+            for (int i = 0; i < Content.Count; i++)
+            {
+                var line = Content[i].Trim(Trims);
+
+                if (line.StartsWith("#"))
+                {
+                    line = line.TrimStart('#');
+                    line = line.TrimStart(Trims);
+
+                    Content.RemoveAt(i);
+
+                    var spl = line.Split(new[] { ' ' }, 2);
+                    var keyword = spl[0].ToLower();
+
+                    if (keyword == "import" && !skipline)
+                    {
+                        Import(spl[1], i);
+                        Run();
+                        return;
+                    }
+                    else if (keyword == "if")
+                    {
+                        if (!IsDefined(spl[1]))
+                        {
+                            skipline = true;
+                        }
+                    }
+                    else if (keyword == "endif")
+                    {
+                        skipline = false;
+                    }
+                    else if (keyword == "define")
+                    {
+                        AddDefine(spl[1].Trim(Trims));
+                    }
+                    else if (keyword == "undef")
+                    {
+                        RemoveDefine(spl[1].Trim(Trims));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"=! WARN [{Path.GetFileName(FilePath)}:{i}?]: Unknown Keyword \"{keyword}\". Is it mis-typed?");
+                    }
+
+                    i--;
+                }
+
+                if (skipline)
+                {
+                    Content.RemoveAt(i);
+                    i--;
+
+                    if (i + 1 >= Content.Count)
+                    {
+                        Console.WriteLine($"=! WARN [{Path.GetFileName(FilePath)}:{i}?]: #IF is not closed properly.");
+                    }
+                }
+            }
+        }
+
+        public string GetContent()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in Content)
+            {
+                builder.AppendLine(item);
+            }
+            return builder.ToString();
+        }
+
+        void AddDefine(string arg)
+        {
+            foreach (var item in Defines)
+            {
+                if (arg.ToLower() == item.ToLower())
+                    return;
+            }
+            Defines.Add(arg);
+        }
+
+        void RemoveDefine(string arg)
+        {
+            Defines.Remove(arg);
+        }
+
+        bool IsDefined(string name)
+        {
+            name = name.ToLower();
+            foreach (var item in Defines)
+            {
+                if (item.ToLower() == name)
+                    return true;
+            }
+            return false;
+        }
+
+        void Import(string argument, int index)
+        {
+            Console.WriteLine($"Importing {argument} at {index}...");
+            if (argument.StartsWith("\"") && argument.EndsWith("\""))
+            {
+                var filename = argument.Trim('"');
+                if (!Path.IsPathRooted(filename))
+                {
+                    filename = Path.Combine(Path.GetDirectoryName(FilePath), filename);
+                }
+                var proc = new CodePreProcessor(filename);
+                proc.Defines = Defines;
+                proc.Run();
+                var temp = proc.Content;
+                Content.InsertRange(index, temp);
+            }
+            else
+            {
+                throw new Exception($"Wrong Syntax [{Path.GetFileName(FilePath)}:{index}]: Incomplited String");
+            }
+        }
+    }
+}
---
+++ sksh/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sksh
+{
+    class Program
+    {
+        static string GetAbsolute(string path)
+        {
+            if (Path.IsPathRooted(path))
+                return path;
+            return Path.Combine(Environment.CurrentDirectory, path.Replace('/', '\\'));
+        }
+
+        static void Main(string[] args)
+        {
+            string filename = null;
+
+            bool voutFlag = false;
+            string voutArg = null;
+
+            bool foutFlag = false;
+            string foutArg = null;
+
+            bool helpFlag = false;
+
+            bool defFlag = false;
+            List<string> Defines = new List<string>();
+
+            foreach (var item in args)
+            {
+                if (voutFlag)
+                {
+                    voutArg = item;
+                    voutFlag = false;
+                }
+                else if (foutFlag)
+                {
+                    foutArg = item;
+                    foutFlag = false;
+                }
+                else if (defFlag)
+                {
+                    Defines.Add(item.Trim());
+                    defFlag = false;
+                }
+                else if (item.StartsWith("-") || item.StartsWith("/"))
+                {
+                    var keyword = item.TrimStart('-', '/').Trim().ToLower();
+                    switch (keyword)
+                    {
+                        case "vout":
+                            voutFlag = true;
+                            break;
+                        case "fout":
+                            foutFlag = true;
+                            break;
+                        case "def":
+                            defFlag = true;
+                            break;
+                        case "help":
+                            helpFlag = true;
+                            break;
+                        case "?":
+                            helpFlag = true;
+                            break;
+
+                    }
+                }
+                else
+                {
+                    filename = item;
+                }
+            }
+
+            var header = new[]
+            {
+                "Spiky Shader Language Pre-Processor (sksh)",
+                "Version 0.1",
+                "Project Spiky (c) 2018",
+                "-- Spiky ShaderLang (.SKSH) is file format that is an extension of GLSL (OpenGL Shading language). --",
+                "",
+            };
+
+            foreach (var item in header)
+            {
+                Console.WriteLine(item);
+            }
+
+            if (helpFlag)
+            {
+                var helps = new[]
+                {
+                    "= Helps ======================================",
+                    "= help    | Show Help Messages",
+                    "= vout    | Vertex Shader Output Name",
+                    "= fout    | Fragment Shader Output Name",
+                    "= def     | Defines Custom Keywords",
+                    "",
+                    "= Usages =====================================",
+                    "= $ sksh.exe main.sksh -vout mainv.sksh -fout mainf.sksh -def SOMENICE -def NEWDEFINES",
+                    "",
+                    "= Reserved Defines ===========================",
+                    "= Vertex Shader   | VERTEX, GEO, GEOMETRY",
+                    "= Fragment Shader | PIXEL, FRAG, FRAGMENT",
+                    ""
+                };
+
+                foreach (var item in helps)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            if (foutArg != null)
+            {
+                Console.WriteLine("Fragment Shader Processing...");
+                try
+                {
+                    var code = new CodePreProcessor(filename);
+                    code.Defines.Add("PIXEL");
+                    code.Defines.Add("FRAG");
+                    code.Defines.Add("FRAGMENT");
+                    code.Defines.AddRange(Defines);
+                    code.Run();
+                    var content = code.GetContent();
+                    var outfile = GetAbsolute(foutArg);
+                    Console.WriteLine($"Output File Path: {outfile}");
+                    File.WriteAllText(outfile, content);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n=! Error is occur while fragment shader processing! \n=! {ex.Message}\n");
+                    #if DEBUG
+                    throw ex;
+                    #endif
+                }
+            }
+
+            if (voutArg != null)
+            {
+                Console.WriteLine("Vertex Shader Processing...");
+                try
+                {
+                    var code = new CodePreProcessor(filename);
+                    code.Defines.Add("VERTEX");
+                    code.Defines.Add("GEO");
+                    code.Defines.Add("GEOMETRY");
+                    code.Defines.AddRange(Defines);
+                    code.Run();
+                    var content = code.GetContent();
+                    var outfile = GetAbsolute(voutArg);
+                    Console.WriteLine($"Output File Path: {outfile}");
+                    File.WriteAllText(outfile, content);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n=! Error is occur while vertex shader processing! \n=! {ex.Message}\n");
+                    #if DEBUG
+                    throw ex;
+                    #endif
+                }
+            }
+
+#if DEBUG
+            Console.Write("Press Enter To Exit");
+            Console.ReadLine();
+#endif
+        }
+    }
+}
